c9c35fbcaf65776bfa8c60a89e5db4252e1a1c1c
Georgy Marchuk <gmarcuk@gmail.com>
2019-05-26 00:39:09 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,57 @@
+import Plugin from '@swup/plugin';
+
+const arrayify = (list) => Array.prototype.slice.call(list);
+
+export default class ScriptsPlugin extends Plugin {
+	name = 'ScriptsPlugin';
+
+	constructor(options) {
+		super();
+
+		const defaultOptions = {
+			head: true,
+			body: true
+		};
+
+		this.options = {
+			...defaultOptions,
+			...options
+		};
+	}
+
+	mount() {
+		this.swup.on('contentReplaced', this.runScripts);
+	}
+
+	unmount() {
+		this.swup.off('contentReplaced', this.runScripts);
+	}
+
+	runScripts = () => {
+		const scope =
+			this.options.head && this.options.body
+				? document
+				: this.options.head
+				? document.head
+				: document.body;
+
+		const scripts = arrayify(scope.querySelectorAll('script:not([data-swup-ignore-script])'));
+
+		scripts.forEach((script) => this.runScript(script));
+
+		this.swup.log(`Executed ${scripts.length} scripts.`);
+	};
+
+	runScript = (originalElement) => {
+		const element = document.createElement('script');
+
+		for (const { name, value } of arrayify(originalElement.attributes)) {
+			element.setAttribute(name, value);
+		}
+		element.textContent = originalElement.textContent;
+		element.setAttribute('async', 'false');
+
+		originalElement.replaceWith(element);
+		return element;
+	};
+}
